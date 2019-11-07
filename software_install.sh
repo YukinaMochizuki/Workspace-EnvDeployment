3f6e93faea25b1694efff83e12d0e87da5ff698b
YukinaMochizuki <1p41p4jejo@gmail.com>
2019-11-08 01:17:20 +0800

    Update software_install.sh

--- software_install.sh
+++ software_install.sh
@@ -32,6 +32,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node # "node" is an alias for the latest version
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
 # pip and pipenv
 sudo apt install -y python3-pip
