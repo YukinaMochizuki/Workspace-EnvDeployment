62359557ce11a960d33a7b5fc5b69323875cc8d8
mochizuki <1p41p4jejo@gmail.com>
2020-03-20 02:21:35 +0800

    Update zsh theme to powerlevel10k

--- zsh_install.sh
+++ zsh_install.sh
@@ -12,5 +12,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # powerlevel9k
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+p10k configure
